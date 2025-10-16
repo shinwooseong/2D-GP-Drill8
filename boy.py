8fefa4d00d568d8a03b431e8944e844d0d67cdba
shinwooseong <sillyday01@gmail.com>
2025-10-16 21:31:51 +0900

    수업 시간 진행한 완성한 idle,sleep,run 구현

---
+++ boy.py
@@ -0,0 +1,128 @@
+from pico2d import load_image, get_time
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+
+from state_machine import StateMachine
+
+# 이벤트 체크 함수
+
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+    pass
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+    pass
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+    pass
+
+class Idle:
+
+    def __init__(self, boy):
+        self.boy = boy
+
+    def enter(self, e):
+        self.boy.wait_start_time = get_time()
+
+    def exit(self, e):
+        pass
+
+    def do(self):
+        self.boy.frame = (self.boy.frame + 1) % 8
+        if get_time() - self.boy.wait_start_time > 2:
+            self.boy.state_machine.handle_state_event(('TIME_OUT', None))
+
+    def draw(self):
+        if self.boy.face_dir == 1: # right
+            self.boy.image.clip_draw(self.boy.frame * 100, 300, 100, 100, self.boy.x, self.boy.y)
+        else: # face_dir == -1: # left
+            self.boy.image.clip_draw(self.boy.frame * 100, 200, 100, 100, self.boy.x, self.boy.y)
+
+
+
+class Sleep:
+
+    def __init__(self, boy):
+        self.boy = boy
+
+    def enter(self, e):
+        self.boy.dir = 0
+
+    def exit(self, e):
+        pass
+
+    def do(self):
+        self.boy.frame = (self.boy.frame + 1) % 8
+
+    def draw(self):
+        if self.boy.face_dir == 1: # right
+            self.boy.image.clip_composite_draw(self.boy.frame * 100, 300, 100, 100, 3.141592/2, '',self.boy.x-25, self.boy.y-25, 100, 100)
+        else: # face_dir == -1: # left
+            self.boy.image.clip_composite_draw(self.boy.frame * 100, 200, 100, 100, -3.141592/2, '',self.boy.x-25, self.boy.y-25, 100, 100)
+
+
+class Run:
+
+    def __init__(self, boy):
+        self.boy = boy
+
+    def enter(self,e):
+        if right_down(e) or left_up(e) :
+            self.boy.dir = self.boy.face_dir = 1
+        elif left_down(e) or right_up(e):
+            self.boy.dir = self.boy.face_dir = -1
+
+    def exit(self,e):
+        pass
+
+    def do(self):
+        self.boy.frame = (self.boy.frame + 1) % 8
+        self.boy.x += self.boy.dir * 5
+
+
+    def draw(self):
+        if self.boy.face_dir == 1: # right
+            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x, self.boy.y)
+        else: # face_dir == -1: # left
+            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y)
+
+
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.face_dir = 1
+        self.dir = 1
+        self.image = load_image('animation_sheet.png')
+
+        self.IDLE = Idle(self)
+        self.SLEEP = Sleep(self)
+        self.RUN = Run(self)
+        self.state_machine = StateMachine(
+            self.IDLE, # 시작 상태
+            {
+                self.SLEEP: {space_down: self.IDLE},
+                self.IDLE: {time_out: self.SLEEP, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN},
+                self.RUN: {right_up: self.IDLE, left_up: self.IDLE, left_down: self.IDLE, right_down: self.IDLE}
+            }
+        )
+
+    def update(self):
+        self.state_machine.update()
+
+
+    def draw(self):
+        self.state_machine.draw()
+
+    def handle_event(self, event):
+        self.state_machine.handle_state_event(('INPUT', event))
---
+++ state_machine.py
@@ -0,0 +1,26 @@
+from event_to_string import event_to_string
+
+class StateMachine:
+    def __init__(self, start_state, rules):
+        self.next_state = None
+        self.cur_state = start_state
+        self.rules = rules
+        self.cur_state.enter(('START', None))
+
+    def update(self):
+        self.cur_state.do()
+
+    def draw(self):
+        self.cur_state.draw()
+
+    def handle_state_event(self, state_event):
+        for check_event in self.rules[self.cur_state].keys():
+            if check_event(state_event):
+                self.cur_state.exit(state_event)
+                self.next_state = self.rules[self.cur_state][check_event]
+                self.next_state.enter(state_event)
+                print(f'{self.cur_state.__class__.__name__} - {event_to_string(state_event)} -> {self.next_state.__class__.__name__}')
+                self.cur_state = self.next_state
+                return
+        print(f'처리되지 않은 이벤트 {event_to_string(state_event)} 가 발생.')
+
