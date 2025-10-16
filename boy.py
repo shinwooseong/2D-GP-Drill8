2cde61db7f48e5d448f2fdb6d3d5c42458df1438
shinwooseong <sillyday01@gmail.com>
2025-10-16 21:40:10 +0900

    기존 class Run 복사해서 AutoRun 클래스 틀 제작

--- boy.py
+++ boy.py
@@ -102,6 +102,31 @@ class Run:
 
 
 
+class AutoRun:
+
+    def __init__(self, boy):
+        self.boy = boy
+
+    def enter(self,e):
+        if right_down(e) or left_up(e) :
+            self.boy.dir = self.boy.face_dir = 1
+        elif left_down(e) or right_up(e):
+            self.boy.dir = self.boy.face_dir = -1
+
+    def exit(self,e):
+        pass
+
+    def do(self):
+        self.boy.frame = (self.boy.frame + 1) % 8
+        self.boy.x += self.boy.dir * 5
+
+
+    def draw(self):
+        if self.boy.face_dir == 1: # right
+            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x, self.boy.y)
+        else: # face_dir == -1: # left
+            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y)
+
 
 class Boy:
     def __init__(self):
